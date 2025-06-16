35480b8513e62263821ac91d67d498452441a96e
pheyboer <heyboer.peter@gmail.com>
2025-06-16 19:45:36 -0400

    adding localstorage for reservation service

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -5,7 +5,13 @@ import { Reservation } from '../models/reservation';
   providedIn: 'root',
 })
 export class ReservationService {
+
   private reservations: Reservation[] = [];
+
+  constructor(){
+    let savedReservations = localStorage.getItem("reservations");
+    this.reservations = savedReservations? JSON.parse(savedReservations) : [];
+  }
 
   //CRUD
 
@@ -19,12 +25,13 @@ export class ReservationService {
 
   addReservation(reservation: Reservation): void {
     this.reservations.push(reservation);
-    console.log(this.reservations);
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   deleteReservation(id: string): void {
     let index = this.reservations.findIndex((res) => res.id === id);
     this.reservations.splice(index, 1);
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   updateReservation(updatedReservation: Reservation): void {
@@ -32,5 +39,6 @@ export class ReservationService {
       (res) => res.id === updatedReservation.id
     );
     this.reservations[index] = updatedReservation;
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 }
