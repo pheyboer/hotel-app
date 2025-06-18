cbe46432ac2eb09b33466fa239bd1069f337e901
pheyboer <heyboer.peter@gmail.com>
2025-06-18 09:30:44 -0400

    showing all reservations in a list

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
 @Component({
   selector: 'app-reservation-list',
   standalone: true,
@@ -9,6 +10,14 @@ import { RouterModule } from '@angular/router';
   templateUrl: './reservation-list.component.html',
   styleUrl: './reservation-list.component.css'
 })
-export class ReservationListComponent {
+export class ReservationListComponent implements OnInit {
   
+  reservations: Reservation[] = [];
+
+  constructor(private reservationService: ReservationService){}
+
+  ngOnInit(): void {
+    this.reservations = this.reservationService.getReservation();
+  }
+
 }
