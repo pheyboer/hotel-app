449697253ed6a6cfd5dd261ab8a3d693bcf96958
pheyboer <heyboer.peter@gmail.com>
2025-06-19 10:18:38 -0400

    adding ability to delete reservations

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -18,6 +18,10 @@ export class ReservationListComponent implements OnInit {
 
   ngOnInit(): void {
     this.reservations = this.reservationService.getReservations();
+
+    deleteReservation(id: string){
+      this.reservationService.deleteReservation(id);
+    }
   }
 
 }
